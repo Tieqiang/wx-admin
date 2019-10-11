fb9a1cbb1385ba0191b1d62e5629b659377a02c3
zhaotq <306403031@qq.com>
2019-10-11 14:19:36 +0800

    病人信息登记

---
+++ src/pages/pat/PatEditForm.jsx
@@ -0,0 +1,21 @@
+import React, {Component} from "react";
+import {Form} from "antd";
+
+class PatEditForm extends Component {
+
+  render() {
+    return (
+      <div>
+        <Form>
+
+        </Form>
+      </div>
+    )
+  }
+}
+
+export default Form.create({
+  mapPropsToFields: props => {
+
+  }
+})();
